d31a5534c6d94c8b09184465cee5d2a8c1c3f6d6
jirrazabal <joseirrazabal@gmail.com>
2024-01-26 16:53:18 -0300

    fix cuotas

--- src/cart.js
+++ src/cart.js
@@ -1,71 +1,81 @@
-import React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import bancos from './bancos';
-import productos from './listadeproductos';
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
-const ShoppingCart = ({ cart, onClearCart }) => {
+const ShoppingCart = ({ cart, onClearCart, banco3, banco6 }) => {
+  console.log({ banco3 });
+  console.log({ banco6 });
 
-  const totalPrice = cart.reduce((acc, item) => item.psvp_lista.replace(/[$.]/g,''), 0);
-  const cuota12 = cart.reduce((acc, item) => item.cuota_ah12.replace(/[$.]/g,''), 0);
-  const cuota6 = cart.reduce((acc, item) => item.cuota_ah6.replace(/[$.]/g,''), 0);
-  
-  console.log(totalPrice);
-  console.log(bancos);
-  console.log(productos);
-  console.log(cuota12);
+  const totalPrice = cart.reduce((acc, item) => {
+    const priceNumber = parseInt(item.psvp_lista.replace(/[$.,]/g, ""), 10);
+    return acc + priceNumber;
+  }, 0);
 
-  const subtotal = cuota12;
-  const totalConEnvio = subtotal + 1000;
-  console.log(totalConEnvio);
   return (
-      <div className='fixed-menu flex-center'>
-        <Accordion style={{width: '100%', maxWidth: 500}}>
-          <AccordionSummary
-            expandIcon={<ArrowDropDownIcon />}
-            aria-controls="panel2-content"
-            id="panel2-header"
-            className='accordion'
+    <div className="fixed-menu flex-center">
+      <Accordion style={{ width: "100%", maxWidth: 500 }}>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+          className="accordion"
+        >
+          <Typography>Carrito de Compras</Typography>
+          <Typography fontWeight={800}>Total: {totalPrice}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container
+            maxWidth="lg"
+            className="flex-center"
+            style={{ flexDirection: "column", padding: "0px 0 20px 0" }}
           >
-            <Typography>Carrito de Compras</Typography>
-            <Typography fontWeight={800}>Total: {totalPrice}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Container maxWidth="lg" className='flex-center' style={{flexDirection: 'column', padding: '0px 0 20px 0'}}>
+            <div>
+              <ul>
+                {cart.map((item) => (
+                  <li key={item.codigo}>
+                    {item.descripcion} - ${item.psvp_lista}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex-center" style={{ flexDirection: "column" }}>
               <div>
-                <ul>
-                  {cart.map((item) => (
-                    <li key={item.codigo}>{item.descripcion} - ${item.psvp_lista}</li>
-                  ))}
-                </ul>
+                <h3>Planes de Pago</h3>
               </div>
-              <div className='flex-center' style={{flexDirection: 'column'}}>
-                <div>
-                  <h3>Planes de Pago</h3>
-                </div>
-                <div className='flex-between'>
+              <div className="flex-between">
+                {banco3?.ahora3 && (
                   <div>
                     <Typography fontWeight={900}>cuotas sin interes</Typography>
-                    <Typography><b>Ahora 6</b> ${cuota6}</Typography>
+                    <Typography>
+                      <b>Ahora 3</b> ${(totalPrice / 3).toFixed(2)}
+                    </Typography>
                   </div>
+                )}
+                {banco6?.ahora6 && (
                   <div>
-                    <Typography fontWeight={900}>cuotas con  interes</Typography>
-                    <Typography><b>Ahora 12</b> ${cuota12}</Typography>
+                    <Typography fontWeight={900}>cuotas con interes</Typography>
+                    <Typography>
+                      <b>Ahora 6</b> ${(totalPrice / 6).toFixed(2)}
+                    </Typography>
                   </div>
-                </div>
-              <div style={{marginTop: 20}}>
-              <Button variant='contained' onClick={onClearCart}>Limpiar carrito</Button>
+                )}
               </div>
+              <div style={{ marginTop: 20 }}>
+                <Button variant="contained" onClick={onClearCart}>
+                  Limpiar carrito
+                </Button>
               </div>
-            </Container>
-          </AccordionDetails>
-        </Accordion>
-      </div>
+            </div>
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 };
 export default ShoppingCart;
+
--- src/home.js
+++ src/home.js
@@ -1,141 +1,175 @@
-import React, { useEffect, useState } from 'react';
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
-import ShoppingCart from './cart';
-import bancos from './bancos';
-import Product from './products';
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+import Skeleton from "@mui/material/Skeleton";
+import ShoppingCart from "./cart";
+import bancos from "./bancos";
+import Product from "./products";
 
-const Home = ({onLogout}) => {
-    const url = "https://backtest-production.up.railway.app"
-    const [cart, setCart] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [productos, setProductos] = useState([])
-    const [filtro, setFiltro] = useState('');
-    const [productosFiltrados, setProductosFiltrados] = useState([]);
-  
-    const [isSticky, setIsSticky] = useState(false);
+const Home = ({ onLogout }) => {
+  const url = "https://backtest-production.up.railway.app";
 
-    useEffect(()=> {
-        const getData = async() => {
-            const result = await axios.get(`${url}/api/productos`) 
-            setProductos(result.data)
-            setProductosFiltrados(result.data)
-        }
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [productos, setProductos] = useState([]);
+  const [filtro, setFiltro] = useState("");
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
 
-        getData()
-    }, [])
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 100) {
-          setIsSticky(true);
-        } else {
-          setIsSticky(false);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    useEffect(() => {
-      const productosFiltrados = productos.filter(producto =>
-        producto.descripcion.toLowerCase().includes(filtro.toLowerCase())
-      );
-  
-      setProductosFiltrados(productosFiltrados);
-      setLoading(false);
-    }, [filtro, productos]);
-  
-    const addToCart = (productos) => {
-      setCart([...cart, productos]);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const [selectedBank3, setSelectedBank3] = useState("");
+  const [selectedBank6, setSelectedBank6] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(`${url}/api/productos`);
+      setProductos(result.data);
+      setProductosFiltrados(result.data);
     };
-  
-    const clearCart = () => {
-      setCart([]);
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
     };
-    return (
-        <Container maxWidth="lg" className='conteiner-list'>
-            <div className='flex-center'>
-                <h2>Catalogo de Productos y precios</h2>
-            </div>
-            <button onClick={onLogout}>Logout</button>
-            <div className={`header flex-center pad20 ${isSticky ? 'sticky' : ''}`}>
-            <TextField 
-                style={{maxWidth: 450}}
-                fullWidth
-                className='search'
-                id="outlined-basic" 
-                label="Buscar Producto" 
-                variant="outlined" 
-                value={filtro}
-                onChange={(e) => setFiltro(e.target.value)} 
-            />
-            </div>
-            <div className='flex-center' style={{padding: '20px 0'}}>        
-            <FormControl fullWidth>
-                <Grid container spacing={2}>
-                <Grid item sm={6} xs={12}>
-                    <div className='w-100'>
-                    <InputLabel id="demo-simple-select-label">3 cuotas</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        fullWidth
-                        style={{background: 'white'}}
-                        value={bancos.codigo}
-                        label="Hola"
-                    >
-                        {bancos.map(bank => (
-                        bank.ahora3 === true ? <MenuItem value={10}>{bank.banco}</MenuItem> : null
-                        ))}
-                    </Select>
-                    </div>
-                </Grid>
-                <Grid item sm={6} xs={12}>
-                    <div className='w-100'>
-                    <InputLabel id="demo-simple-select-label">6 cuotas</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        fullWidth
-                        style={{background: 'white'}}
-                        value={bancos.codigo}
-                        label="Age"
-                    >
-                        {bancos.map(bank => (
-                        bank.ahora6 === true ? <MenuItem value={10}>{bank.banco}</MenuItem> : null
-                        ))}
-                    </Select>
-                    </div>
-                </Grid>
-                </Grid>
-            </FormControl>
-            </div>
-            <ul className='lista-prod'>
-            {loading ? <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" /> :
-            (
-                productosFiltrados.map(product => (
-                  product.vigencia === 'SI' ? <li className='grid-item' key={product.id}>
-                  <Product key={product.codigo} product={product} onAddToCart={addToCart} /> 
-                  </li> : null
-              ))
-            )}
-            </ul>
-            <ShoppingCart cart={cart} onClearCart={clearCart} />
-        </Container>
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const productosFiltrados = productos.filter((producto) =>
+      producto.descripcion.toLowerCase().includes(filtro.toLowerCase()),
     );
+
+    setProductosFiltrados(productosFiltrados);
+    setLoading(false);
+  }, [filtro, productos]);
+
+  const addToCart = (productos) => {
+    setCart([...cart, productos]);
   };
-  export default Home;
+
+  const clearCart = () => {
+    setCart([]);
+  };
+  return (
+    <Container maxWidth="lg" className="conteiner-list">
+      <div className="flex-center">
+        <h2>Catalogo de Productos y precios</h2>
+      </div>
+      <button onClick={onLogout}>Logout</button>
+      <div className={`header flex-center pad20 ${isSticky ? "sticky" : ""}`}>
+        <TextField
+          style={{ maxWidth: 450 }}
+          fullWidth
+          className="search"
+          id="outlined-basic"
+          label="Buscar Producto"
+          variant="outlined"
+          value={filtro}
+          onChange={(e) => setFiltro(e.target.value)}
+        />
+      </div>
+      <div className="flex-center" style={{ padding: "20px 0" }}>
+        <Grid container spacing={2}>
+          <Grid item sm={6} xs={12}>
+            <div className="w-100">
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">3 cuotas</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  fullWidth
+                  style={{ background: "white" }}
+                  value={selectedBank3}
+                  onChange={(e, child) => {
+                    e.preventDefault();
+                    setSelectedBank3(e.target.value);
+                  }}
+                >
+                  {bancos.map((bank, i) =>
+                    bank.ahora3 === true ? (
+                      <MenuItem value={bank.codigo} key={i}>
+                        {bank.banco}
+                      </MenuItem>
+                    ) : null,
+                  )}
+                </Select>
+              </FormControl>
+            </div>
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <div className="w-100">
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">6 cuotas</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  fullWidth
+                  style={{ background: "white" }}
+                  value={selectedBank6}
+                  onChange={(e, child) => {
+                    e.preventDefault();
+                    setSelectedBank6(e.target.value);
+                  }}
+                >
+                  {bancos.map((bank, i) =>
+                    bank.ahora6 === true ? (
+                      <MenuItem value={bank.codigo} key={i}>
+                        {bank.banco}
+                      </MenuItem>
+                    ) : null,
+                  )}
+                </Select>
+              </FormControl>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <ul className="lista-prod">
+        {loading ? (
+          <Skeleton
+            sx={{ height: 190 }}
+            animation="wave"
+            variant="rectangular"
+          />
+        ) : (
+          productosFiltrados.map((product) =>
+            product.vigencia === "SI" ? (
+              <li className="grid-item" key={product.id}>
+                <Product
+                  key={product.codigo}
+                  product={product}
+                  onAddToCart={addToCart}
+                />
+              </li>
+            ) : null,
+          )
+        )}
+      </ul>
+      <ShoppingCart
+        cart={cart}
+        onClearCart={clearCart}
+        banco3={bancos.find((bank) => bank.codigo === selectedBank3)}
+        banco6={bancos.find((bank) => bank.codigo === selectedBank6)}
+      />
+    </Container>
+  );
+};
+export default Home;
