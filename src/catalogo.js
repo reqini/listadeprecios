07a05ee7fadb22f68333e95a63290c90cf08b349
luciano <reqini@gmail.com>
2024-08-27 09:14:14 -0300

    catalogo online 2

--- src/catalogo.js
+++ src/catalogo.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Skeleton from "@mui/material/Skeleton";
-import ProductsCalatogo from "./productsCalatogo";
+import ProductsCalatogo from "./products";
 import { Typography } from "@mui/material";
 
 const Catalogo = () => {
@@ -15,7 +15,14 @@ const Catalogo = () => {
   const [filtro, setFiltro] = useState("");
   const [productosFiltrados, setProductosFiltrados] = useState([]);
   const [isSticky, setIsSticky] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false); // Estado para controlar la vista de favoritos
 
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success');
+
+  // Cargar productos desde la API
   useEffect(() => {
     const getData = async () => {
       const result = await axios.get(`${url}/api/productos`);
@@ -27,14 +34,11 @@ const Catalogo = () => {
     getData();
   }, []);
 
+  // Manejar scroll para hacer sticky el header
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-      if (offset > 100) {
-        setIsSticky(true);
-      } else {
-        setIsSticky(false);
-      }
+      setIsSticky(offset > 100);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -44,24 +48,61 @@ const Catalogo = () => {
     };
   }, []);
 
+  // Filtrar productos por descripción
   useEffect(() => {
     const productosFiltrados = productos.filter((producto) =>
-      producto.descripcion.toLowerCase().includes(filtro.toLowerCase()),
+      producto.descripcion.toLowerCase().includes(filtro.toLowerCase())
     );
 
     setProductosFiltrados(productosFiltrados);
   }, [filtro, productos]);
 
-  const addToCart = (productos) => {
-    setCart([...cart, productos]);
+  // Cargar favoritos desde localStorage al montar el componente
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  // Añadir producto al carrito
+  const addToCart = (product) => {
+    setCart([...cart, product]);
   };
-  
+
+  // Manejar el agregado y eliminación de favoritos
+  const toggleFavorite = (product) => {
+    let updatedFavorites;
+    let message;
+
+    if (favorites.some(fav => fav.id === product.id)) {
+      // Si el producto ya está en favoritos, lo eliminamos
+      updatedFavorites = favorites.filter(fav => fav.id !== product.id);
+      message = `${product.descripcion} ha sido eliminado de tus favoritos`;
+      setSnackbarSeverity('warning');
+    } else {
+      // Si no está en favoritos, lo agregamos
+      updatedFavorites = [...favorites, product];
+      message = `${product.descripcion} ha sido agregado a tus favoritos`;
+      setSnackbarSeverity('success');
+    }
+
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    
+    // Mostrar el Snackbar con el mensaje
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+
+  // Filtrar los productos que se deben mostrar
+  const productosAMostrar = showFavorites 
+    ? productosFiltrados.filter(product => favorites.some(fav => fav.id === product.id))
+    : productosFiltrados;
 
   return (
     <Container maxWidth="lg" className="conteiner-list">
       <div className="flex-between-mobile" style={{ paddingTop: 30 }}>
-        <Typography variant="h5" textAlign="center" width={"100%"} margin={'15px 0'}>
-          Catalogo de Productos
+        <Typography variant="h5" textAlign="center" width={"100%"}>
+          Catalogo de Productos y precios
         </Typography>
       </div>
       <div className={`header-catalogo flex-center pad10 ${isSticky ? "sticky" : ""}`}>
@@ -76,6 +117,15 @@ const Catalogo = () => {
           onChange={(e) => setFiltro(e.target.value)}
         />
       </div>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={() => setShowFavorites(!showFavorites)}
+        style={{ marginBottom: '20px' }}
+        disabled={favorites.length === 0} // Deshabilitar si no hay favoritos
+      >
+        {showFavorites ? 'Mostrar Todos' : 'Mostrar Favoritos'}
+      </Button>
       <ul className="lista-prod w-100">
         {loading ? (
           <>
@@ -97,41 +147,37 @@ const Catalogo = () => {
               variant="rectangular"
               className="grid-item"
             />
-            <Skeleton
-              sx={{ height: 300, margin: 1 }}
-              animation="wave"
-              variant="rectangular"
-              className="grid-item"
-            />
-            <Skeleton
-              sx={{ height: 300, margin: 1 }}
-              animation="wave"
-              variant="rectangular"
-              className="grid-item"
-            />
-            <Skeleton
-              sx={{ height: 300, margin: 1 }}
-              animation="wave"
-              variant="rectangular"
-              className="grid-item"
-            />
+            {/* Renderiza más Skeletons mientras carga */}
           </>
         ) : (
-          productosFiltrados.map((product) =>
+          productosAMostrar.map((product) =>
             product.vigencia === "SI" ? (
               <li className="grid-item" key={product.id}>
                 <ProductsCalatogo
                   key={product.codigo}
                   product={product}
-                  catalog
                   onAddToCart={addToCart}
+                  isFavorite={favorites.some(fav => fav.id === product.id)}
+                  onToggleFavorite={() => toggleFavorite(product)}
                 />
               </li>
-            ) : null,
+            ) : null
           )
         )}
       </ul>
+
+      {/* Snackbar para mostrar mensajes */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity} sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
+
 export default Catalogo;
--- src/productsCalatogo.js
+++ src/productsCalatogo.js
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 // Product.js
+=======
+>>>>>>> 37e2c14 (catalogo online)
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { FaWhatsapp } from 'react-icons/fa';
+<<<<<<< HEAD
 import { Button, CardActions } from '@mui/material';
 
 
@@ -59,6 +63,66 @@ import { Button, CardActions } from '@mui/material';
           }
         </CardActions>
       </Card>
+=======
+import { Button, CardActions, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const ProductsCalatogo = ({ product, isFavorite, onToggleFavorite }) => {
+
+  const createWhatsAppLink = (product) => {
+    const message = `¡Hola!, Te envio el valor de tu próxima Essen!
+      Producto: ${product.descripcion}`;
+
+    return `https://api.whatsapp.com/send?text=${encodeURIComponent(message)}`;
+  };
+
+  return (
+    <Card sx={{ maxWidth: 600, paddingBottom: '12px' }} className='card-product-catalogo'>
+      {product.discount && <div className='descuento'>{product.discount}</div>}
+       {/* Botón de favoritos */}
+       <IconButton onClick={onToggleFavorite} color="secondary" style={{position: 'absolute', zIndex: 2, right: 10, top: 10}}>
+          {isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+        </IconButton>
+      <CardMedia
+        component="img"
+        height="220"
+        image={product.imagen ? product.imagen : '../descarga.png'}
+        alt="product"
+      />
+      <CardContent style={{ display: 'flex', flexDirection: 'column', padding: '6px 16px 0 16px' }}>
+        <Typography variant='body2' fontSize={12} fontStyle={'italic'} style={{ marginBottom: 5 }}>
+          Línea <b>{product.linea}</b>
+        </Typography>
+        <Typography className='titulo' lineHeight={1} gutterBottom variant="h6" fontSize={18}>
+          {product.descripcion}
+        </Typography>
+      </CardContent>
+      <CardActions style={{ display: 'flex', flexDirection: 'column' }}>
+        <Button
+          fullWidth
+          href={createWhatsAppLink(product)}
+          target="_blank"
+          variant="contained"
+          size="medium"
+          color="primary"
+          style={{ backgroundColor: '#25D366', color: 'white', margin: '5px 0 12px 0' }}
+          startIcon={<FaWhatsapp />}
+        >
+          Compartir
+        </Button>
+        {product.ficha_tecnica ? (
+          <Button fullWidth target='_blank' href={product.ficha_tecnica} variant="outlined" size="medium" color="primary" style={{ margin: 0 }}>
+            Ficha técnica
+          </Button>
+        ) : (
+          <Button fullWidth variant="disabled" size="medium" color="primary" style={{ margin: 0 }}>
+            Sin ficha técnica
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+>>>>>>> 37e2c14 (catalogo online)
   );
 };
 
