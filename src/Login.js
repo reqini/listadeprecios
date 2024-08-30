d52de823b25e402471ae26e9e5dfd6605a3f15e8
luciano <reqini@gmail.com>
2024-08-30 19:27:00 -0300

    error login

--- src/Login.js
+++ src/Login.js
@@ -18,14 +18,10 @@ const Login = ({ onLogin }) => {
 
   useEffect(() => {
     const savedUser = localStorage.getItem('username');
-    if (savedUser) {
+    if (savedUser && window.location.pathname !== '/home') {
       navigate('/home');
     }
   }, [navigate]);
-
-  const handleChangePass = (e) => {
-    setPassword(e.target.value);
-  };
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
@@ -40,6 +36,9 @@ const Login = ({ onLogin }) => {
 
     // Guarda el usuario en localStorage
     localStorage.setItem('username', username);
+
+    // Reset loading state
+    setLoading(false);
   };
 
   const handleChange = (e) => {
@@ -49,58 +48,58 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div className='full-width' style={{backgroundColor: '#FFEDC4'}}>
-      <Container className='flex justify-center items-center flex-direction' maxWidth="sm" style={{paddingTop:100}}>
+    <div className='full-width' style={{ backgroundColor: '#FFEDC4' }}>
+      <Container className='flex justify-center items-center flex-direction' maxWidth="sm" style={{ paddingTop: 100 }}>
         <img src={logo} alt="logo" height="100" className='mar-b10' />
         <form onSubmit={handleSubmit}>
           <Grid container spacing={0} className='card'>
-              <Grid item xs={12} style={{margin: '10px 0'}}>
-                  <label>
-                      <TextField
-                          required
-                          fullWidth
-                          style={{ color: 'black', backgroundColor: 'white' }}
-                          id="filled-required"
-                          label={'Nombre'}          
-                          defaultValue={username}
-                          variant="filled"
-                          onChange={handleChange}
-                      />
-                  </label>
-              </Grid>
-              <Grid item xs={12} style={{margin: '10px 0'}}>
-                  <label>
-                    <TextField
-                      required
-                      fullWidth
-                      type={showPassword ? 'text' : 'password'} // Cambia el tipo según el estado
-                      style={{ color: 'black', backgroundColor: 'white' }}
-                      id="filled-required"
-                      label="Código de emprendedora"
-                      value={password} // Usa value en lugar de defaultValue para controlar el componente
-                      variant="filled"
-                      onChange={handleChangePass}
-                      InputProps={{
-                        endAdornment: (
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            edge="start"
-                            style={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)' }}
-                          >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        ),
-                      }}
-                    />
-                  </label>
-              </Grid>
-              <Grid item xs={12} style={{margin: '10px 0'}}>
-                  <Button fullWidth type="submit" variant="contained" size="large">
-                      {loading ? 'Cargando' : 'Entrar'}
-                  </Button>
-              </Grid>
-          </Grid>       
+            <Grid item xs={12} style={{ margin: '10px 0' }}>
+              <label>
+                <TextField
+                  required
+                  fullWidth
+                  style={{ color: 'black', backgroundColor: 'white' }}
+                  id="filled-required"
+                  label={'Nombre'}
+                  value={username}
+                  variant="filled"
+                  onChange={handleChange}
+                />
+              </label>
+            </Grid>
+            <Grid item xs={12} style={{ margin: '10px 0' }}>
+              <label>
+                <TextField
+                  required
+                  fullWidth
+                  type={showPassword ? 'text' : 'password'} // Cambia el tipo según el estado
+                  style={{ color: 'black', backgroundColor: 'white' }}
+                  id="filled-required"
+                  label="Código de emprendedora"
+                  value={password} // Usa value en lugar de defaultValue para controlar el componente
+                  variant="filled"
+                  onChange={e => setPassword(e.target.value)}
+                  InputProps={{
+                    endAdornment: (
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="start"
+                        style={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)' }}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </label>
+            </Grid>
+            <Grid item xs={12} style={{ margin: '10px 0' }}>
+              <Button fullWidth type="submit" variant="contained" size="large">
+                {loading ? 'Cargando...' : 'Entrar'}
+              </Button>
+            </Grid>
+          </Grid>
         </form>
       </Container>
     </div>
