4238d52e193df4cd75d5399f068aab2e9c47353d
luciano <reqini@gmail.com>
2024-09-01 10:51:06 -0300

    agregados plan, carrito compartir

--- src/cart.js
+++ src/cart.js
@@ -82,7 +82,7 @@ const ShoppingCart = ({ cart, onClearCart }) => {
                 color="primary"
                 href={createWhatsAppLink()}
                 target="_blank"
-                style={{ backgroundColor: '#25D366', color: 'white', margin: '12px 0', display: 'none' }}
+                style={{ backgroundColor: '#25D366', color: 'white', margin: '12px 0' }}
                 startIcon={<FaWhatsapp />}
               >
                 Compartir por WhatsApp
