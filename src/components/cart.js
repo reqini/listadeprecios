7cf4c5a49839e34a8635662c02bc20dcb8df649e
luciano <reqini@gmail.com>
2024-09-06 16:36:57 -0300

    se coulta whatsapp de carrito

--- src/components/cart.js
+++ src/components/cart.js
@@ -155,7 +155,7 @@ const ShoppingCart = ({ cart, onClearCart, onRemoveFromCart }) => { // Recibimos
                 color="primary"
                 href={createWhatsAppLink()}
                 target="_blank"
-                style={{ backgroundColor: '#25D366', color: 'white', margin: '12px 0' }}
+                style={{ backgroundColor: '#25D366', color: 'white', margin: '12px 0', display: 'none' }}
                 startIcon={<FaWhatsapp />}
               >
                 Compartir por WhatsApp
