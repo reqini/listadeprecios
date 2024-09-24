2af33056c3a3e4602120e1d028285ea6d23fee85
luciano <reqini@gmail.com>
2024-09-24 19:56:49 -0300

    solo promos 3

--- src/components/products.js
+++ src/components/products.js
@@ -68,9 +68,9 @@ const Product = ({ product, onAddToCart, catalog = false }) => {
             )}
           </Select>
         </FormControl>
-         { product.linea === 'Bazar'| 'Complementos'| 'Repuestos' ?
-          <Typography variant="body2" color="red">Solo con promos bancarias</Typography> : null
-        }
+        {['Bazar', 'Complementos', 'Repuestos'].includes(product.linea) ? (
+          <Typography variant="body2" color="red">Solo con promos bancarias</Typography>
+        ) : null}
       </CardContent>
 
       <CardActions sx={{ display: 'flex', flexDirection: 'column' }}>
