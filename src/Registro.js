f3a0b50b0c50253b7f8b3d8ae3a1cc56e09114b2
Luciano Recchini <lucianorecchini@MacBook-Air-de-Natalia.local>
2024-10-24 14:43:33 -0300

    registro revert 3

--- src/Registro.js
+++ src/Registro.js
@@ -10,7 +10,7 @@ const Register = () => {
     setLoading(true);
   
     try {
-      const response = await fetch("https://script.google.com/macros/s/AKfycbwC55WPsHzAoabUdvfLVc-K5_EhPyWWK9MDJYzox9JFSzI82t7rv6x1MwQ3ecTX0wGdlQ/exec", {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbxCJDtRtuoN_4dsySbQRJcCHcmILiIFPJ08LsByGtSXwYj-aNj3C1C9DTGah3TrAC734w/exec", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
