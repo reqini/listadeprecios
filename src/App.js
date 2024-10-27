c9475df64180fff605644020d9ea3df8208f4501
Luciano Recchini <lucianorecchini@MacBook-Air-de-Natalia.local>
2024-10-27 15:18:12 -0300

    merge estable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Register from "./Registro";
 import Login from "./Login";
 import Home from "./home";
 import Catalogo from "./catalogo";
@@ -36,19 +35,7 @@ const App = () => {
         <Router>
           <Routes>
             <Route path="/login" element={<LoginRoute />} />
-            <Route path="/registro" element={<Register />} />
-<<<<<<< Updated upstream
             <Route path="/home" element={<PrivateRoute component={Home} />} />
-            <Route path="/catalogo" element={<PrivateRoute component={Catalogo} />} />
-            <Route path="/catalogo3" element={<PrivateRoute component={Catalogo3} />} />
-            <Route path="/catalogo6" element={<PrivateRoute component={Catalogo6} />} />
-            <Route path="/catalogo9" element={<PrivateRoute component={Catalogo9} />} />
-            <Route path="/catalogo12" element={<PrivateRoute component={Catalogo12} />} />
-            <Route path="/catalogo18" element={<PrivateRoute component={Catalogo18} />} />
-            <Route path="/catalogo20" element={<PrivateRoute component={Catalogo20} />} />
-            <Route path="/catalogo24" element={<PrivateRoute component={Catalogo24} />} />
-=======
-            <Route path="/home" element={<PrivateRoute component={Home} handleLogout={handleLogout} />} />
             <Route path="/catalogo" element={<Catalogo />} />  {/* Ruta libre */}
             <Route path="/catalogo3" element={<Catalogo3 />} />  {/* Ruta libre */}
             <Route path="/catalogo6" element={<Catalogo6 />} />  {/* Ruta libre */}
@@ -57,7 +44,6 @@ const App = () => {
             <Route path="/catalogo18" element={<Catalogo18 />} />  {/* Ruta libre */}
             <Route path="/catalogo20" element={<Catalogo20 />} />  {/* Ruta libre */}
             <Route path="/catalogo24" element={<Catalogo24 />} />  {/* Ruta libre */}
->>>>>>> Stashed changes
             <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
         </Router>
@@ -74,11 +60,7 @@ const PrivateRoute = ({ component: Component }) => {
     return <Navigate to="/login" />;
   }
 
-<<<<<<< Updated upstream
   return <Component />;
-=======
-  return <Component handleLogout={handleLogout} />;
->>>>>>> Stashed changes
 };
 
 // Ruta de login que redirige al usuario a "home" si ya está autenticado
