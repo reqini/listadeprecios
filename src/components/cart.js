5ab30e55ba90a756836be93a32ec147f0840814f
Luciano Recchini <lucianorecchini@MacBook-Air-de-Luciano.local>
2024-12-06 15:37:10 -0300

    envio test 3

--- src/components/cart.js
+++ src/components/cart.js
@@ -123,7 +123,7 @@ const ShoppingCart = ({ cart, onClearCart, setCart, onRemoveFromCart }) => {
   };
 
   // Determinar si mostrar el switch de envío
-  const totalPoints = calculateTotalPoints();
+/*   const totalPoints = calculateTotalPoints(); */
   /* const showShippingSwitch = totalPoints < 140; */ // Ahora depende exclusivamente de los puntos totales
 
   // Manejo de la eliminación del producto del carrito y resetear cuotas/planCanje
