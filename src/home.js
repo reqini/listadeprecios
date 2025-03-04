d48f4d5d17a9376c54af6894f8c1923b2a181d9c
Luciano Recchini <lucianorecchini@MacBook-Air-de-Luciano.local>
2025-03-04 10:41:21 -0300

    quien la hizo y banner 2

--- src/home.js
+++ src/home.js
@@ -241,7 +241,7 @@ const Home = () => {
       <Container maxWidth="lg" className="conteiner-list">
         
         <div className="w-100 flex justify-center items-center flex-direction mar-t10">
-          <Typography fontSize={13} margin={'6px 0 12px 0'}>Desarrollado por: <b><a href="https://www.instagram.com/lrecchini/" target="_blank">Luciano Recchini</a></b></Typography>
+          <Typography fontSize={13} margin={'6px 0 12px 0'}>Desarrollado por: <b><a href="https://www.instagram.com/lrecchini/" rel="noreferrer">Luciano Recchini</a></b></Typography>
           <img src={logo} alt="logo" width="200" className="mar-t10 mar-b20" />
         </div>
 
