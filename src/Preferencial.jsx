853becedf9c3a6a0bae5da56f028fab07959eb15
Luciano <reqini@gmail.com>
2025-12-06 21:47:06 -0300

    fix: Agregar import de useRef en Preferencial.jsx

--- src/Preferencial.jsx
+++ src/Preferencial.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo, useRef } from "react";
 import axios from "./utils/axios";
 import Container from "@mui/material/Container";
 import Skeleton from "@mui/material/Skeleton";
